7f749dfc11aad48ede1beb94229df54d5b0c20ba
romarage <rrudych@gmail.com>
2016-02-28 17:44:43 +0200

    Initial commit

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by Roman on 28.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello world");
+    }
+}
