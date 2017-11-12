20d34a29a42fbddd9494df572a061d4ffc7af43e
= <=>
2017-11-12 18:52:56 +0200

    First commit in dev branch

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,13 +1,12 @@
 package test;
 
-/**
- * Created by Roman on 28.02.2016.
- */
 public class Main {
 
     public static void main(String[] args) {
         System.out.printf("Hello world");
         System.out.println("New text");
         System.out.println("More text");
+
+	System.out.println("Text for first commit");
     }
 }
